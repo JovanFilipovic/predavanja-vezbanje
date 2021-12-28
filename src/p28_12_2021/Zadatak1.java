1a9bdc96432a2ce9c8678d8fa4a4d5dd3f027007
Jovan Filipovic <jovanfilipovicsport@gmail.com>
2021-12-28 19:51:14 +0100

    Array print helper needed

---
+++ src/p28_12_2021/Zadatak1.java
@@ -0,0 +1,24 @@
+package p28_12_2021;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] array = {1,2,3,4,1,2,3,31,4,5,67,7,8,346,34534,734734,854,2,324,623,6,2};
+		
+		printArray(array);
+		
+		
+	}
+
+	
+	public static void printArray(int[] array) {
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+	}
+	
+}
